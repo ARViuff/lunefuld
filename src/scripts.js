6828af5d5b9bf715dc3ffb0b480a9f14ed6f96d1
Anders Riis Viuff <ande528c@wicket.com>
2022-09-28 18:39:05 +0200

    3rd

--- src/scripts.js
+++ src/scripts.js
@@ -1,34 +1,53 @@
 // Navigation
 const toggleSearch = () => {
-    document.getElementById("searchInput").classList.toggle("hidden");
+  document.getElementById("searchInput").classList.toggle("hidden");
 };
 const mdOptionsToggle = () => {
-    document.getElementById("md-searchbar").classList.toggle("hidden");
-    document.getElementById("md-searchbar").classList.toggle("flex");
+  document.getElementById("md-searchbar").classList.toggle("hidden");
+  document.getElementById("md-searchbar").classList.toggle("flex");
 };
 const openMenu = () => {
-    document.getElementById("mobile-menu").classList.remove("hidden");
+  document.getElementById("mobile-menu").classList.remove("hidden");
 };
 const closeMenu = () => {
-    document.getElementById("mobile-menu").classList.add("hidden");
+  document.getElementById("mobile-menu").classList.add("hidden");
 };
 // Navigation slut
 
 function showFilters() {
-    var fSection = document.getElementById("filterSection");
-    if (fSection.classList.contains("hidden")) {
-        fSection.classList.remove("hidden");
-        fSection.classList.add("block");
-    } else {
-        fSection.classList.add("hidden");
-    }
+  var fSection = document.getElementById("filterSection");
+  if (fSection.classList.contains("hidden")) {
+    fSection.classList.remove("hidden");
+    fSection.classList.add("block");
+  } else {
+    fSection.classList.add("hidden");
+  }
 }
 
 function applyFilters() {
-    document.querySelectorAll("input[type=checkbox]").forEach((el) => (el.checked = false));
+  document
+    .querySelectorAll("input[type=checkbox]")
+    .forEach((el) => (el.checked = false));
 }
 
 function closeFilterSection() {
-    var fSection = document.getElementById("filterSection");
-    fSection.classList.add("hidden");
+  var fSection = document.getElementById("filterSection");
+  fSection.classList.add("hidden");
 }
+
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks() {
+  cookieboks.classList.add("hidden");
+  sessionStorage.setItem("fjerncookieboks", true);
+}
+/*
+if (sessionStorage.getItem("fjerncookieboks")) {
+  cookieboks.classList.add("hidden");
+}
+*/
